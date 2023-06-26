592e863de547eaa07c5046a5e877504ded3fe6c9
Hussein Okasha <111140118+hussien415532@users.noreply.github.com>
2023-06-26 14:11:51 +0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,10 @@
 let showResult = document.querySelector(".show");
-let inputs = Array.from(document.querySelectorAll("input"));
+let inputs = document.querySelectorAll("input");
 let dayInput = document.querySelector(".day input");
+let drkbdy = document.querySelector("body");
+let resDark = document.querySelectorAll(".result p");
+let darlabel = document.querySelectorAll(".input-date p.m-1");
+let darkBox = document.querySelector(".box");
 let monthInput = document.querySelector(".month input");
 let yearInput = document.querySelector(".year input");
 let dayValid = document.querySelector(".validDay");
@@ -9,6 +13,7 @@ let yearValid = document.querySelector(".validYear");
 let year = document.querySelector(".yearResult");
 let month = document.querySelector(".monthResult");
 let day = document.querySelector(".dayResult");
+let toggler = document.querySelector(".toggle");
 let date = new Date();
 
 inputs.forEach((inp) => {
@@ -34,7 +39,11 @@ showResult.addEventListener("click", () => {
     monthValid.style.display = "none";
     monthInput.style.borderColor = "hsl(0, 0%, 86%)";
   }
-  if (yearInput.value > date.getFullYear() || yearInput.value == "" ||yearInput.value<1950) {
+  if (
+    yearInput.value > date.getFullYear() ||
+    yearInput.value == "" ||
+    yearInput.value < 1950
+  ) {
     yearValid.style.display = "block";
     yearInput.style.borderColor = "red";
   } else {
@@ -47,23 +56,36 @@ showResult.addEventListener("click", () => {
   calcAge(birthDate);
 });
 
+toggler.addEventListener("click", () => {
+  
+  drkbdy.classList.toggle("dark-body");
+  darkBox.classList.toggle("dark");
+  inputs.forEach((inp) => {
+    inp.classList.toggle("dark-input");
+  });
+  darlabel.forEach((lbl) => {
+    lbl.classList.toggle("dark-text");
+  });
+  resDark.forEach((r) => {
+    r.classList.toggle("dark-text");
+  });
+});
+
 function calcAge(bd) {
- 
   let diff = date - bd;
   let age = new Date(diff);
   let y = age.getFullYear() - 1970;
-  if(y > 90)
-  {
+  if (y > 90) {
     return 0;
   }
-  let  m = age.getMonth();
-  let  d = age.getDate() -1;
+  let m = age.getMonth();
+  let d = age.getDate() - 1;
   year.innerHTML = Math.trunc(y);
   month.innerHTML = Math.trunc(m);
   day.innerHTML = Math.trunc(d);
 }
-let d =  new Date("99-1-1");
-let ddd= date - d;
+let d = new Date("99-1-1");
+let ddd = date - d;
 let r = new Date(ddd);
 
-console.log(r.getFullYear()-1970)
+console.log(r.getFullYear() - 1970);
